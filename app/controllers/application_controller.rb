6c2b642004588778ef891d2780fbe15e7eb56209
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-19 16:28:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,25 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+set :views, Proc.new { File.join(root, "../views/") }
+    
+  get '/' do
+    erb :form
+  end
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/new' do
+    erb :'pirates/new'
+  end
 
-    get '/' do
-      erb :form
-    end
+
+post '/pirates' do
+  @pirate = Pirate.new(params[:pirate])
+  params[:pirate][:ships].each do |details|
+  Ship.new(details)
+end
+@ships = Ship.all
+erb :'pirates/show'
+end
 
 
 end
